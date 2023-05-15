2816ab6c70b950f366ac5400e4bcd0d22112b5c8
Tim Ruscica <mrtechguy234@gmail.com>
2023-05-15 20:30:30 -0300

    add header

--- src/app/page.js
+++ src/app/page.js
@@ -100,6 +100,7 @@ export default function Game() {
 
   return (
     <div className="game">
+      <h1>Testing auto deployment</h1>
       <div className="game-board">
         <Board squares={currentSquares} xIsNext={xIsNext} onPlay={handlePlay} />
       </div>
